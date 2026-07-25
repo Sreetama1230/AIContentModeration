36b23495218fc336649175fa44e637c9219beae8
Sreetama Saha <sreetamasaha2417@gmail.com>
2026-07-25 19:47:01 +0530

    added the token in the env

---
+++ src/test/java/com/ai/contentmoderation/app/AiContentModerationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.contentmoderation.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiContentModerationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
